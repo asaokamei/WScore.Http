77b0d183d695896bf5c34bbc8b431e995cfe27e6
Asao <kamei@workspot.jp>
2013-08-15 08:08:19 +0900

    copy test files from Web\Http.

---
+++ tests/autoload.php
@@ -0,0 +1,18 @@
+<?php
+namespace WScore\tests;
+if( defined( 'VENDOR_DIRECTORY' ) ) { return; }
+elseif( file_exists( __DIR__ . '/../vendor/' ) ) {
+    define( 'VENDOR_DIRECTORY', __DIR__ . '/../vendor/' );
+}
+elseif( file_exists( __DIR__ . '/../../../../vendor/' ) ) {
+    define( 'VENDOR_DIRECTORY', __DIR__ . '/../../../../vendor/' );
+}
+else {
+    die( 'vendor directory not found' );
+}
+require_once( VENDOR_DIRECTORY . 'autoload.php' );
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->add( 'WScore\Web', dirname( __DIR__ ).'/src' );
+$loader->add( 'WScore\Response', dirname( __DIR__ ).'/src' );
+$loader->add( 'WScore\tests', __DIR__ );
+$loader->register();
